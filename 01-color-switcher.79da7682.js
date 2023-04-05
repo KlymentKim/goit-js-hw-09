4a9d42948e1bac4db80a9cc4dd438b24b05651ce
KlymentKim <49413026+KlymentKim@users.noreply.github.com>
2023-04-05 21:43:06 +0000

    Deploying to gh-pages from @ KlymentKim/goit-js-hw-09@c118407657bde7b3cf2a850b4604860d66bfc2b9 🚀

---
+++ 01-color-switcher.79da7682.js
@@ -0,0 +1,2 @@
+setTimeout((function(){}),1e3);
+//# sourceMappingURL=01-color-switcher.79da7682.js.map
---
+++ 01-color-switcher.8429b961.js
@@ -0,0 +1,2 @@
+setTimeout((()=>{}),1e3);
+//# sourceMappingURL=01-color-switcher.8429b961.js.map
