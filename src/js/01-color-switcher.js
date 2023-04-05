e85ffa6507c676d34830b0458e27a4c11118053c
Klyment <klim86kr@gmail.com>
2023-04-06 00:51:39 +0300

    add 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,11 @@
 
 setTimeout(()=>{
-
+    const buttonStart = document.querySelector('[data-start]');
+    const buttonStop = document.querySelector('[data-stop]');
+    const changeColorStart= document.body.style.backgroundColor= getRandomHexColor();
+    buttonStart.innerHTML=changeColorStart;
 },1000);
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
