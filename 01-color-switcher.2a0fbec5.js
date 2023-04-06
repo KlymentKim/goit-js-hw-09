3d95b81991ed76a426f0f622722f3f3484461a69
KlymentKim <49413026+KlymentKim@users.noreply.github.com>
2023-04-06 11:56:15 +0000

    Deploying to gh-pages from @ KlymentKim/goit-js-hw-09@6518fc9aec389019deae936d8bb7345eb9fccb9b 🚀

---
+++ 01-color-switcher.2a0fbec5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(t=>{t.preventDefault(),setTimeout((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),500)}));
+//# sourceMappingURL=01-color-switcher.2a0fbec5.js.map
