6518fc9aec389019deae936d8bb7345eb9fccb9b
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-04-06 14:55:40 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,19 @@
 
-setTimeout(()=>{
-    const buttonStart = document.querySelector('[data-start]');
-    const buttonStop = document.querySelector('[data-stop]');
-    const changeColorStart= document.body.style.backgroundColor= getRandomHexColor();
-    buttonStart.innerHTML=changeColorStart;
-},1000);
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+buttonStart.addEventListener('click', (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+        const changeColorRandmize = getRandomHexColor();
+        document.body.style.backgroundColor = changeColorRandmize;
+        
+    }, 500);
+    // buttonStart.innerHTML = changeColorRandmize;
+});
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
   }
