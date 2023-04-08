fcd9fd2fd7d60d6f98082df4ec4b033ad3babab7
Klyment <klim86kr@gmail.com>
2023-04-09 01:59:18 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,21 @@
-
-const buttonStart = document.querySelector('[data-start]');
-const buttonStop = document.querySelector('[data-stop]');
-
-buttonStart.addEventListener('click', (event) => {
-    event.preventDefault();
-    setTimeout(() => {
-        const changeColorRandmize = getRandomHexColor();
-        document.body.style.backgroundColor = changeColorRandmize;
-        
-    }, 500);
-    // buttonStart.innerHTML = changeColorRandmize;
-});
-
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
         .toString(16)
         .padStart(6, 0)}`;
   }
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId= null;
+
+const changeBodyColor = () =>{
+    const setColor = getRandomHexColor();
+    document.body.style.backgroundColor = setColor;
+    setTimeout(changeBodyColor,1000);
+};
+buttonStart.addEventListener('click', changeBodyColor);
+
+const stopChangeColor = () =>{
+    clearTimeout(null);
+};
+buttonStop.addEventListener('click', stopChangeColor);
