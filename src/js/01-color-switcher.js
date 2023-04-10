baf6f103b033b7c8d214c3c7b433863f9427bd16
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-04-10 16:59:14 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,19 +3,42 @@ function getRandomHexColor() {
         .toString(16)
         .padStart(6, 0)}`;
   }
-const buttonStart = document.querySelector('[data-start]');
-const buttonStop = document.querySelector('[data-stop]');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
-let intervalId= null;
+// let intervalId= null;
 
-const changeBodyColor = () =>{
-    const setColor = getRandomHexColor();
-    document.body.style.backgroundColor = setColor;
-    setTimeout(changeBodyColor,1000);
-};
-buttonStart.addEventListener('click', changeBodyColor);
+// const changeBodyColor = () =>{
+//     const setColor = getRandomHexColor();
+//     document.body.style.backgroundColor = setColor;
+//     setTimeout(changeBodyColor, 1000);
+//     if (buttonStart) {
+//         buttonStart.disable = true;
+//     }
+// };
+// buttonStart.addEventListener('click', changeBodyColor);
 
-const stopChangeColor = () =>{
-    clearTimeout(null);
-};
-buttonStop.addEventListener('click', stopChangeColor);
+// const stopChangeColor = () =>{
+//     clearTimeout(intervalId);
+// };
+// buttonStop.addEventListener('click', stopChangeColor);
+  let intervalId = null;
+
+  startBtn.addEventListener('click', () => {
+    if (!intervalId) {
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+      intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    }
+  });
+
+  stopBtn.addEventListener('click', () => {
+    if (intervalId) {
+      startBtn.disabled = false;
+      stopBtn.disabled = true;
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+  });
