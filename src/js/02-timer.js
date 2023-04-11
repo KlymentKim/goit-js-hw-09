285da283a50aaa92102527a00a5531874bffb6d5
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-04-11 10:42:18 +0300

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,23 +8,6 @@ const hours = document.querySelector("[data-hours]");
 const minutes = document.querySelector("[data-minutes]");
 const seconds = document.querySelector("[data-seconds]");
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
-// function convertMs(ms) {
-//     // Number of milliseconds per unit of time
-//     const second = 1000;
-//     const minute = second * 60;
-//     const hour = minute * 60;
-//     const day = hour * 24;
-// }
-
 let countdown;
 
 const options = {
